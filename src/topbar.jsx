6994ac5ba6dec695129d1ba08954b649b7a88a52
Ganeshmahi47 <shgane468@gmail.com>
2022-09-30 02:35:33 +0530

    new

--- src/topbar.jsx
+++ src/topbar.jsx
@@ -10,6 +10,8 @@ return(
             <h5 class="card-title text-muted text-uppercase text-center">Free</h5>
             <h6 class="card-price text-center">$0<span class="period">/month</span></h6>
             <hr/>
+
+            
             <ul class="fa-ul">
               <li><span class="fa-li"><i class="fas fa-check"></i></span>Single User</li>
               <li><span class="fa-li"><i class="fas fa-check"></i></span>5GB Storage</li>
