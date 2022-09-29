b8364827d1642e8fd775e6927957dc1d39c2b52c
Ganeshmahi47 <shgane468@gmail.com>
2022-09-30 02:31:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './card';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Topbar from './topbar';
+function App() {
+  return (
+    <div className='container py3' >
+     <header>
+     <Topbar></Topbar>
+     </header> 
+      
+      
+    </div>
+  );
+}
+
+export default App;
